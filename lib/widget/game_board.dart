29ff9246ea97d992880a249509832659d93f8a97
dadum <sir.dadum@gmail.com>
2023-12-11 20:27:00 +0100

    Remove unused import

--- lib/widget/game_board.dart
+++ lib/widget/game_board.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:snake/bloc/game_bloc.dart';
 import 'package:snake/widget/board_tile.dart';
 
