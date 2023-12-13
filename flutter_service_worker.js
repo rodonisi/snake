073f8b745a9098ba89a02642fd4043a472e137d3
rodonisi <52073517+rodonisi@users.noreply.github.com>
2023-12-13 20:25:47 +0000

    Deploying to gh-pages from @ rodonisi/snake@b7be7feaaf8b8bd7789c364178f0974ab51c0890 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "0847adbc93b6fc891b9648f886871a0e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "35d63792c8f4824650daf7b1442e3abd",
-"/": "35d63792c8f4824650daf7b1442e3abd",
-"main.dart.js": "c2b348b32c5cd64efdd9e17f2c3451cf",
+"index.html": "60ba86fc23fd6c32ca5ca0fbc3fdb178",
+"/": "60ba86fc23fd6c32ca5ca0fbc3fdb178",
+"main.dart.js": "5c81ff60e4d8d51fdc95e168da29fcb4",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
